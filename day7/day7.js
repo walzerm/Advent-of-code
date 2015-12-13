a37dbeba8020e27960b84e51671dbef6fb4e8d59
Lissa Walzer <lissawalzer@gmail.com>
2015-12-12 21:53:20 -0800

    Added the input file for day7

---
+++ day7/day7.js
@@ -0,0 +1,13 @@
+var fs = require('fs');
+
+fs.readFile('input.txt', "utf8", function(err, data) {
+    var instructionArr = data.split("\n");
+    console.log(instructionArr);
+
+    //console.log(nMyNumber.toString(2));//convert decimal to binary
+
+
+})
+
+
+//console.log(123 & 456);
