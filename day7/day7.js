219e39cb83d3237da6eff657189fd7414de17408
Lissa Walzer <lissawalzer@gmail.com>
2015-12-13 21:07:45 -0800

    Needs more work

--- day7/day7.js
+++ day7/day7.js
@@ -2,9 +2,15 @@ var fs = require('fs');
 
 fs.readFile('input.txt', "utf8", function(err, data) {
     var instructionArr = data.split("\n");
+    //console.log(instructionArr);
+    instructionArr.sort(function(a, b) {
+        return a.length - b.length;
+    })
     console.log(instructionArr);
-
-    //console.log(nMyNumber.toString(2));//convert decimal to binary
+    var circuit = {};
+    //for (var i = 0; i < instructionArr.length - 1; i++) {
+    //    instructionArr[i]
+    //}    //console.log(nMyNumber.toString(2));//convert decimal to binary
 
 
 })
